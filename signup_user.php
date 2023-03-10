497aa4434bf6b908bab66199f393e27f27a98693
rungtom003 <68326591+rungtom003@users.noreply.github.com>
2023-03-10 09:48:53 +0700

    Rung (#22)
    * add carenda
    
    * add ER
    
    * add book
    
    * edit full carendar
    
    * classroom_timetable
    
    * edit all
    
    * edit ref
    
    * user_signup
    
    * Merge branch 'main' of https://github.com/rungtom003/RoomBook into rung
    
    * book delete
    
    * add map
    
    * delete export data
    
    * add comment
    
    * add img

--- signup_user.php
+++ signup_user.php
@@ -33,7 +33,8 @@ $active_home = "active";
         align-items: center;
         justify-content: center;
         padding: 20px;
-        background: rgb(255, 255, 255);
+        background-image: url("./src/img/S__34299923.jpg");
+        background-size: cover;
     }
 
     .container {
